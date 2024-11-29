58d2f3cfa7000b628dfb03978bed1c7e6020e563
Srinu427 <srinivas.adigopula@gmail.com>
2024-11-29 20:35:08 +0530

    add initial physics engine

--- physics/src/lib.rs
+++ physics/src/lib.rs
@@ -17,7 +17,7 @@ pub struct PhysicsEngine<const S: usize ,const D: usize> {
 }
 
 impl<const S: usize ,const D: usize> PhysicsEngine<S,D> {
-  pub fn new() -> Self<S,D> {
+  pub fn new() -> Self {
     Self {
       static_objects: FnvIndexMap::new(),
       dynamic_objects: FnvIndexMap::new(),
@@ -27,25 +27,48 @@ impl<const S: usize ,const D: usize> PhysicsEngine<S,D> {
   }
 
   pub fn add_static_polygon(&mut self, name: &str, polygon: PolygonMesh) -> Result<(), String> {
-    for (do_id, sep_list) in self.dyn_static_separations.iter_mut() {
-      sep_list.push(self.dynamic_objects[do_id].get_separation_plane(&polygon))
-        .map_err(|_| "max static objects count reached".to_string())?;
+    for (do_id, sep_map) in self.dyn_static_separations.iter_mut() {
+      sep_map
+        .insert(
+          name.to_string(),
+          self.dynamic_objects[do_id].get_separation_plane(&polygon)
+        )
+          .map_err(|_| "max static objects count reached".to_string())?;
     }
     self
       .static_objects
       .insert(name.to_string(), polygon)
-      .map_err(|_| "max static objects count reached".to_string())
+      .map_err(|_| "max static objects count reached".to_string())?;
+    Ok(())
   }
 
-  pub fn add_dynamic_polygon(&mut self, polygon: PolygonMesh) -> Result<(), String> {
+  pub fn add_dynamic_polygon(&mut self, name: &str, polygon: PolygonMesh) -> Result<(), String> {
     self
       .dyn_static_separations
-      .push(self.static_objects.iter().map(|so| polygon.get_separation_plane(so)).collect())
+      .insert(
+        name.to_string(),
+        self
+          .static_objects
+          .iter()
+          .map(|(so_name, so)| (so_name.clone(), polygon.get_separation_plane(so)))
+          .collect()
+      )
       .map_err(|_| "max dynamic objects count reached".to_string())?;
     self
       .dyn_static_separations
-      .push(self.dynamic_objects.iter().map(|dyn_o| polygon.get_separation_plane(dyn_o)).collect())
+      .insert(
+        name.to_string(),
+        self
+          .dynamic_objects
+          .iter()
+          .map(|(dyno_name, dyn_o)| (dyno_name.to_string(), polygon.get_separation_plane(dyn_o)))
+          .collect()
+      )
       .map_err(|_| "max dynamic objects count reached".to_string())?;
-    self.dynamic_objects.push(polygon).map_err(|_| "max dynamic objects count reached".to_string())
+    self
+      .dynamic_objects
+      .insert(name.to_string(), polygon)
+      .map_err(|_| "max dynamic objects count reached".to_string())?;
+    Ok(())
   }
 }
